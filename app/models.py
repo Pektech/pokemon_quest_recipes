24e118285a34093da24ab6238e883fd1fbe473e1
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-07-09 23:37:10 -0400

    added assoc model for recipes, ingredients and quantities

--- app/models.py
+++ app/models.py
@@ -2,14 +2,21 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from app import db
 
 
-recipe_ingreds = db.Table('recipe_ingreds',
-                db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
-                db.Column('ingred_id', db.Integer, db.ForeignKey('ingredient.id')))
+
+# recipe_ingreds = db.Table('recipe_ingreds',
+#                 db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
+#                 db.Column('ingred_id', db.Integer, db.ForeignKey('ingredient.id')),
+#                 db.Column('quantity', db.Integer))
 
 creature_recipe = db.Table('creature_recipe',
                 db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
                 db.Column('creature_id', db.Integer, db.ForeignKey('creature.id')))
 
+class Association(db.Model):
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), primary_key=True)
+    ingred_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), primary_key=True)
+    quantity = db.Column(db.Integer, default=1)
+    child = db.relationship('Ingredient')
 
 
 class Recipe(db.Model):
@@ -17,10 +24,10 @@ class Recipe(db.Model):
     name = db.Column(db.String(100), index=True, unique=True)
     rating = db.Column(db.String(12), unique=True)
 
-    ingredients = db.relationship("Ingredient",
-                                  secondary=recipe_ingreds)
-    ingredient_name = association_proxy('ingredient', 'name')
+    ingredients = db.relationship("Association")
 
+    ingredient_name = association_proxy('ingredients', 'child.name')
+    ingredient_quantity = association_proxy('ingredients','quantity')
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
